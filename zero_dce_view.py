5b8a2ae45d1cd8a474570ef02dc4dbed82d17a0a
Khalid Farhan <khalid.farhan12@gmail.com>
2025-05-05 10:24:49 +0700

    update tampilan wakti di zero dce

--- zero_dce_view.py
+++ zero_dce_view.py
@@ -47,7 +47,7 @@ if uploaded_image is not None:
         with col2:
             st.image(enhanced_img, caption=f"Enhanced image")
 
-        st.write(f"Waktu proses: {execution_time:.3f} detik")
+        st.write(f"Waktu proses: {execution_time} detik")
 
         metrics_data = pd.DataFrame({
             "Metrics": ["NIQE", "BRISQUE"],
